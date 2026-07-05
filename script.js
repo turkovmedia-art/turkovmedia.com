82beed87acf3008f0264c9240005e017e5a3c41c
shmouelbetsaleldahan <shmouelbetsaleldahan@gmail.com>
2026-07-05 01:55:18 -0400

    Fix EN switcher overlap using flexbox flow spacing, fix contact section center text alignment class, reduce header to categories margin gap on mobile, and apply fullscreen viewport overrides

--- script.js
+++ script.js
@@ -1005,7 +1005,7 @@ function openVideoPlayer(project) {
         container.innerHTML = `
             <div class="plyr__video-embed" id="custom-youtube-player" style="width: 100%; height: 100%;">
                 <iframe
-                    src="https://www.youtube.com/embed/${ytId}?origin=${window.location.origin}&iv_load_policy=3&modestbranding=1&playsinline=1&showinfo=0&rel=0&enablejsapi=1&autoplay=1"
+                    src="https://www.youtube.com/embed/${ytId}?origin=${window.location.origin}&iv_load_policy=3&modestbranding=1&controls=1&playsinline=1&showinfo=0&rel=0&enablejsapi=1&autoplay=1"
                     allowfullscreen
                     allowtransparency
                     allow="autoplay; fullscreen"
